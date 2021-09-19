94db4edd83e5ca84f84345d5c356636e3120830b
Jarred Muller <j.muller@live.com.au>
2021-09-19 19:07:44 +1000

    add password checker

---
+++ password_check.py
@@ -0,0 +1,11 @@
+MIN_LENGTH = 2
+password = input('Set Password:')
+confirm = False
+while confirm == False:
+    if len(password) < MIN_LENGTH:
+        print('Please enter password of appropriate length.')
+        password = input('Set Password:')
+    else:
+        for i in range(0, len(password)):
+            print('*', end=' ')
+        confirm = True
